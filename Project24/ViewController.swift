70b43c3d00c62c13bc1e4d25b44b23b8d9bed80e
vasko34 <vasko_botsev_34@abv.bg>
2023-11-11 11:18:21 +0200

    Initial Commit

---
+++ Project24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project24
+//
+//  Created by User on 11.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
